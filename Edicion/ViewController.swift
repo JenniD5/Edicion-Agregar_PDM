3046c9bc2671e0c4f21da3973aa45bae0542e4f0
Araceli Arizmendi <araceli.muse@gmail.com>
2021-10-28 16:20:39 -0700

    Initial Commit

---
+++ Edicion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Edicion
+//
+//  Created by Alumno on 10/28/21.
+//  Copyright © 2021 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
